e47ce78fb15d6f5c2235cb7b4ef03e6fe18f5a52
coderitual <skowronkow@gmail.com>
2021-08-12 17:14:49 +0200

    Organize sources

--- block-isolated.js
+++ block-isolated.js
@@ -1,13 +1,7 @@
-const lighthouse = require('lighthouse');
-const puppeteer = require('puppeteer');
 const argv = require('yargs').argv;
-const chromeLauncher = require('chrome-launcher');
-const reportGenerator = require('lighthouse/report/report-generator');
-const request = require('request');
-const util = require('util');
-const fs = require('fs');
-const path = require('path');
-const convertUrlToFilename = require('./convertUrlToFilename');
+const convertUrlToFilename = require('./lib/convertUrlToFilename');
+const lighthouseFromPuppeteer = require('./lib/lighthouseFromPuppeteer');
+const saveToCsv = require('./lib/saveToCsv');
 
 // CLI arguments
 const url = argv.url ?? 'https://brainly.com/question/1713545';
@@ -58,69 +52,6 @@ const config = {
   },
 };
 
-// function creates directoy if it doesn't exist
-function ensureDir(dirPath) {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-  }
-}
-
-async function lighthouseFromPuppeteer(url, options, config = null) {
-  // Launch chrome using chrome-launcher
-  const chrome = await chromeLauncher.launch(options);
-  options.port = chrome.port;
-
-  // Connect chrome-launcher to puppeteer
-  const resp = await util.promisify(request)(
-    `http://localhost:${options.port}/json/version`,
-  );
-  const { webSocketDebuggerUrl } = JSON.parse(resp.body);
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: webSocketDebuggerUrl,
-  });
-
-  // Run Lighthouse
-  const { lhr, report } = await lighthouse(url, options, config);
-
-  const json = reportGenerator.generateReport(lhr, 'json');
-
-  const audits = JSON.parse(json).audits; // Lighthouse audits
-
-  const first_contentful_paint =
-    audits['first-contentful-paint'].numericValue.toFixed(2);
-  const largest_contentful_paint =
-    audits['largest-contentful-paint'].numericValue.toFixed(2);
-  const speed_index = audits['speed-index'].numericValue.toFixed(2);
-  const max_potential_fid = audits['max-potential-fid'].numericValue.toFixed(2);
-  const cumulative_layout_shift =
-    audits['cumulative-layout-shift'].numericValue.toFixed(4);
-  const total_blocking_time =
-    audits['total-blocking-time'].numericValue.toFixed(2);
-  const time_to_interactive = audits['interactive'].numericValue.toFixed(2);
-
-  console.log(`\n
-     Lighthouse metrics: 
-     URL: ${url},
-     🎨 First Contentful Paint: ${first_contentful_paint}, 
-     📱 Cumulative Layout Shift: ${cumulative_layout_shift},
-     🌄 Largest Contentful Paint: ${largest_contentful_paint},
-     ⏳ Max Potential FID: ${max_potential_fid},
-     ⌛️ Total Blocking Time: ${total_blocking_time},
-     👆 Time To Interactive: ${time_to_interactive}`);
-
-  await browser.disconnect();
-  await chrome.kill();
-
-  return {
-    first_contentful_paint,
-    cumulative_layout_shift,
-    largest_contentful_paint,
-    max_potential_fid,
-    total_blocking_time,
-    time_to_interactive,
-  };
-}
-
 async function gatherResults(url, options, config) {
   const results = [];
   const patterns = ['', ...blockedUrlPatterns];
@@ -136,40 +67,6 @@ async function gatherResults(url, options, config) {
     }
   }
   return results;
-}
-
-function saveToCSV(filename, url, results) {
-  ensureDir(path.dirname(filename));
-  fs.appendFileSync(
-    filename,
-    `url, blocked_pattern, first_contentful_paint, cumulative_layout_shift, largest_contentful_paint, max_potential_fid, total_blocking_time, time_to_interactive\n`,
-    function (err) {
-      if (err) throw err;
-    },
-  );
-
-  for (const result of results) {
-    const {
-      url,
-      pattern,
-      first_contentful_paint,
-      cumulative_layout_shift,
-      largest_contentful_paint,
-      max_potential_fid,
-      total_blocking_time,
-      time_to_interactive,
-    } = result;
-
-    fs.appendFileSync(
-      filename,
-      `${url}, ${
-        pattern || '<no-pattern>'
-      }, ${first_contentful_paint}, ${cumulative_layout_shift}, ${largest_contentful_paint}, ${max_potential_fid}, ${total_blocking_time}, ${time_to_interactive}\n`,
-      function (err) {
-        if (err) throw err;
-      },
-    );
-  }
 }
 
 async function main() {
