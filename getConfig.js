a5ef1f6c30bb50238f0bff990b67296ff11b1592
coderitual <skowronkow@gmail.com>
2022-09-20 14:34:54 +0200

    Add new config

--- getConfig.js
+++ getConfig.js
@@ -1,4 +1,13 @@
 // Default mobile config
+
+const PIXEL5_EMULATION_METRICS = {
+  mobile: true,
+  width: 393,
+  height: 851,
+  deviceScaleFactor: 2.8,
+  disabled: false,
+};
+
 const mobileConfig = {
   extends: 'lighthouse:default',
   settings: {
@@ -7,7 +16,9 @@ const mobileConfig = {
     // lighthouse:default is mobile by default
     // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
     skipAudits: ['uses-http2'],
-    emulatedUserAgent: false, // this option might be used to control loading scripts which detecting lh/bots, e.g. cookie consents providers
+    // this option might be used to control loading scripts which detecting lh/bots, e.g. cookie consents providers
+    emulatedUserAgent: 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36',
+    screenEmulation: PIXEL5_EMULATION_METRICS,
   },
   audits: ['metrics/first-contentful-paint-3g'],
   categories: {
