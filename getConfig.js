c01b66633d8fd9a94d420d92561235318da67f51
coderitual <skowronkow@gmail.com>
2022-09-21 21:01:48 +0200

    Add bootstrap

--- getConfig.js
+++ getConfig.js
@@ -14,12 +14,13 @@ const mobileConfig = {
   settings: {
     maxWaitForFcp: 15 * 1000,
     maxWaitForLoad: 35 * 1000,
-    throttling: constants.throttling.desktopDense4G,
+    //throttling: constants.throttling.desktopDense4G,
     // lighthouse:default is mobile by default
     // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
     skipAudits: ['uses-http2'],
     // this option might be used to control loading scripts which detecting lh/bots, e.g. cookie consents providers
-    emulatedUserAgent: 'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36',
+    emulatedUserAgent:
+      'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36',
     screenEmulation: PIXEL5_EMULATION_METRICS,
   },
   audits: ['metrics/first-contentful-paint-3g'],
